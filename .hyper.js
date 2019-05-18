abeec0424a349bd5225d57da1cd0a26f4013664c
itsumoonazicode <dyingkouki@gmail.com>
2019-05-18 22:18:02 +0900

    Update .hyper.js
    起動シェルをbashに変更

--- .hyper.js
+++ .hyper.js
@@ -103,11 +103,11 @@ module.exports = {
     //
     // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+    shell: 'C:\\Windows\\System32\\bash.exe',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
-    shellArgs: ['--login'],
+    shellArgs: ['~', '--login'],
 
     // for environment variables
     env: {},
